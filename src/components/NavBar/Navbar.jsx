ec6f3dedcf0d2bab7590265a64566c2d0406ae0c
PauliGonz <78574915+PauliGonz@users.noreply.github.com>
2021-06-20 22:30:47 -0400

    probando imagen en navbar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -2,6 +2,7 @@ export function NavBar() {
     return (
         <div className= "container">
             <nav class="navbar navbar-expand-md navbar-light bg-light">
+                <img src="./imagen_bandera.png" alt="logo" />
                 <a class="navbar-brand" href="#">Cursos Online Front End</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
