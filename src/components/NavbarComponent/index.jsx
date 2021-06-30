c71ff44162d1247f276a63262c81bc2a5dbfad3d
PauliGonz <78574915+PauliGonz@users.noreply.github.com>
2021-06-29 23:39:51 -0400

    actualizacion CartWidget

--- src/components/NavbarComponent/index.jsx
+++ src/components/NavbarComponent/index.jsx
@@ -1,5 +1,6 @@
 import logo from "./logoNavbar.png"
 import "./style.scss";
+import { CartWidget } from "../CartWidget/CartWidget";
 
 const NavbarComponent = ({ cart}) => {
     return (
@@ -16,12 +17,7 @@ const NavbarComponent = ({ cart}) => {
                         <span className="nav-item nav-link" >Lenguajes</span>
                         <span className="nav-item nav-link" >Frameworks</span>
                         <span className="nav-item nav-link disabled" >Carrito - {cart.length}</span>
-                        <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-bag" viewBox="0 0 16 16">
-                                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
-                            </svg>
-                            <span className="badge badge-dark" id="numero-carrito">0</span>
-                        </div>
+                        <CartWidget />
                     </div>
                 </div>
             </nav>
